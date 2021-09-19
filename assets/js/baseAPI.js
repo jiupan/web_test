662844e03d2b7842623a0e7443a6ab67102d7fd1
deyu <1445245307@qq.com>
2021-09-19 22:45:27 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,19 @@
 $.ajaxPrefilter(function(options) {
+    //统一为所有api地址自动添加前缀网址
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    // 统一为有权限的接口设置header请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
+    //全局统一挂在complate回调函数
+    // 无论成功与否都会调用complete函数
+    options.complete = function(res) {
+        if (res.responseJSON.status !== 0) {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+$(function() {
+    $('#btnLogout').on('click', function() {
+        //eg1
+        layer.confirm('是否退出登陆?', { icon: 3, title: '提示' }, function(index) {
+            // 清空本地存储的localStorage
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            layer.close(index);
+        });
+    })
+
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data);
+        }
+    })
+})
+
+// 渲染用户头像函数
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $("#welcome").html('欢迎&nbsp&nbsp' + name);
+    // 判断用户是否有头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        var first = name[0].toUpperCase();
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(first).show();
+    }
+}
